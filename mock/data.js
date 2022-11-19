be8673e710d231a3ca70eedc336d923cba542cae
imariel2d <imariel2d@gmail.com>
2022-11-18 23:53:38 -0700

    [no-jira] - initial changes / rendering mock data

---
+++ mock/data.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * This is real data from thecatapi. Keeping it here so we don't have to call
+ * the real api for the moment.
+ *
+ * https://api.thecatapi.com/
+ */
+const cats = [
+  {
+    id: "es",
+    url: "https://cdn2.thecatapi.com/images/es.jpg",
+    width: 640,
+    height: 427
+  },
+  {
+    id: "189",
+    url: "https://cdn2.thecatapi.com/images/189.gif",
+    width: 500,
+    height: 281
+  },
+  {
+    id: "dt8",
+    url: "https://cdn2.thecatapi.com/images/dt8.jpg",
+    width: 960,
+    height: 720
+  }
+];
+
+module.exports = {
+  cats,
+};
