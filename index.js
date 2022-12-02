ffb8c20955b834e3a0c6a2f275e76bec0de2e080
imarielpb <ariel.plascencia@parsable.com>
2022-12-02 14:32:40 -0700

    replacing app.js -> index.js

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 
-const app = express();
+const index = express();
 // require('./scheduler');
 require('./db');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+index.set('views', path.join(__dirname, 'views'));
+index.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+index.use(logger('dev'));
+index.use(express.json());
+index.use(express.urlencoded({ extended: false }));
+index.use(cookieParser());
+index.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+index.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+index.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+index.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +38,4 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = index;
