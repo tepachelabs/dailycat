4387836aad49c3979594044419f85034709052e0
Vourpal <hollowxdstorm@gmail.com>
2022-12-06 18:41:33 -0500

    updated with prettier

--- db/startup/insertMany.js
+++ db/startup/insertMany.js
@@ -4,8 +4,9 @@ const { cats } = require('../../mock/data');
 const { Cat } = require('../models/cat');
 
 Cat.insertMany(cats)
-.then((result) => {
-    console.log("The data has been added to the database!")
-}).catch((err) => {
-    console.log(err)
-});
+  .then((result) => {
+    console.log('The data has been added to the database!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const {Cat} = require("../db/models/cat")
+const { Cat } = require('../db/models/cat');
 
 /* GET home page. */
-<<<<<<< HEAD
-router.get('/', function(req, res) {
-  Cat.find({}).then((result) => {
-    res.render('index', { cats: result });
-  }).catch(() => {
-    res.render('error', { cats:[] });
-  });
-=======
 router.get('/', function (req, res) {
-  res.render('index', { cats });
->>>>>>> main
+  Cat.find({})
+    .then((result) => {
+      res.render('index', { cats: result });
+    })
+    .catch(() => {
+      res.render('error', { cats: [] });
+    });
 });
 
 module.exports = router;
