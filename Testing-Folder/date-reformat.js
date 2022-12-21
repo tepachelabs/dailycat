fee62104ee743c4cac275696e713a22f20fbf8d3
Bruno Oros <hollowxdstorm@gmail.com>
2022-12-21 18:45:05 -0500

    fixed dateFormat function and replaced error with index in routes/index.js

--- Testing-Folder/date-reformat.js
+++
@@ -1,9 +0,0 @@
-let dateFormat = () => {
-    cats: result.map((result) => {
-        return {
-          id: result.id,
-          url: result.url,
-          date: moment(result.date).format('MMMM, Do, YYYY'),
-        };
-      })
-}
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const {dateFormat} = require("../utils/date-reformat")
 const { Cat } = require('../db/models/cat');
 
 /* GET home page. */
 router.get('/', function (req, res) {
   Cat.find({})
     .then((result) => {
-      res.render('index', { cats: result });
+      res.render('index', { cats: dateFormat(result) });
     })
     .catch(() => {
-      res.render('error', { cats: [] });
+      res.render('index', { cats: [] });
     });
 });
 
