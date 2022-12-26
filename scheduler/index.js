dcf7504f6645ec0f4205a0689d66f255868c6482
Ariel Plascencia <imariel2d@gmail.com>
2022-12-26 12:45:56 -0700

    Inserting cat every day at the start of the UTC time zone

--- scheduler/index.js
+++ scheduler/index.js
@@ -1,14 +1,27 @@
 const schedule = require('node-schedule');
 const fetchRandomCatImage = require('../services');
-const { cats } = require('../mock/data');
+const { Cat } = require('../db/models/cat');
+
+const rule = new schedule.RecurrenceRule();
+
+rule.hour = 0;
+rule.tz = 'Etc/UTC';
 
 // second | minute | hour | day-of-month | month | day of week
 // https://crontab.guru/#0_*/1_*_*_*
-const job = schedule.scheduleJob('*/10 * * * * *', function () {
+const job = schedule.scheduleJob(rule, function () {
   fetchRandomCatImage().then((res) => {
-    cats.push({
-      ...res.data[0],
-      date: 'some date',
+    const { id, url } = res.data[0];
+
+    const cat = new Cat({
+      id,
+      url,
+      date: Date.now(),
     });
+
+    cat
+      .save()
+      .then(() => console.log('cat inserted to db'))
+      .catch((err) => console.log(err));
   });
 });
