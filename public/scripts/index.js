783724c4e0e1ba2db757cd4aa559a231c3505786
Ariel Plascencia <imariel2d@gmail.com>
2023-01-20 14:09:28 -0700

    WIP - refactored fetching, styling and pagination

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,36 +1,126 @@
-fetch('http://localhost:3000/api/cats?page=2')
-  .then((res) => res.json())
-  .then((result) => {
-    console.log(result);
-  });
+const ITEMS_PER_PAGE = 10;
+const catList = document.getElementById('cat-list');
+const pageContainer = document.getElementById('page-container');
+let currentPage = 1;
+const PAGES_OFFSET = 3;
 
-let currentPage = 0;
-let nextBtn = document.querySelector('.next');
-let prevBtn = document.querySelector('.prev');
-let pages = document.querySelectorAll('.page');
-nextBtn.addEventListener('click', function () {
-  if (currentPage == pages.length - 1) {
-    return;
-  }
-  pages[currentPage++].classList.remove('active');
-  pages[currentPage].classList.add('active');
-});
+/**
+ *
+ * @param {string} date
+ * @param {string} url
+ * @param {string} quote
+ * @returns {HTMLLIElement}
+ */
+const createCatItem = (date, url, quote = 'Random quote for now') => {
+  const item = document.createElement('li');
+  item.className = 'cat-item';
 
-prevBtn.addEventListener('click', function () {
-  if (currentPage == 0) {
-    return;
-  }
-  pages[currentPage--].classList.remove('active');
-  pages[currentPage].classList.add('active');
-});
+  const image = document.createElement('img');
+  const dataWrapper = document.createElement('div');
 
-for (let i = 0; i < pages.length; i++) {
-  pages[i].addEventListener('click', function () {
-    if (currentPage == i) {
-      return;
+  image.className = 'cat-image';
+  image.src = url;
+
+  dataWrapper.className = 'cat-data-wrapper';
+
+  const dateTitle = document.createElement('h2');
+  dateTitle.textContent = date;
+  dateTitle.className = 'cat-date';
+
+  const quoteText = document.createElement('p');
+  quoteText.textContent = quote;
+  quoteText.className = 'cat-quote';
+
+  dataWrapper.appendChild(dateTitle);
+  dataWrapper.appendChild(quoteText);
+
+  item.appendChild(image);
+  item.appendChild(dataWrapper);
+
+  return item;
+};
+
+const createPageItem = (number) => {
+  const item = document.createElement('li');
+  item.className = 'page';
+  item.innerText = number;
+
+  return item;
+};
+
+const assignPagination = () => {
+  // let nextBtn = document.querySelector('.next');
+  // let prevBtn = document.querySelector('.prev');
+  let pages = document.querySelectorAll('.page');
+  // nextBtn.addEventListener('click', function () {
+  //   if (currentPage == pages.length - 1) {
+  //     return;
+  //   }
+  //   pages[currentPage++].classList.remove('active');
+  //   pages[currentPage].classList.add('active');
+  // });
+  //
+  // prevBtn.addEventListener('click', function () {
+  //   if (currentPage == 0) {
+  //     return;
+  //   }
+  //   pages[currentPage--].classList.remove('active');
+  //   pages[currentPage].classList.add('active');
+  // });
+
+  pages.forEach((page, index) => {
+    const pageIndex = parseInt(page.innerText);
+
+    if (pageIndex === currentPage) {
+      page.classList.add('active');
     }
-    pages[currentPage].classList.remove('active');
-    pages[i].classList.add('active');
-    currentPage = i;
+
+    page.addEventListener('click', () => {
+      if (currentPage === pageIndex) {
+        return null;
+      }
+
+      currentPage = pageIndex;
+      fetchPage(pageIndex);
+    });
   });
-}
+
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
+
+/**
+ *
+ * @param {number} pageNumber
+ */
+const fetchPage = (pageNumber = 1) => {
+  pageContainer.innerHTML = '';
+  catList.innerHTML = '';
+
+  fetch(`http://localhost:3000/api/cats?page=${pageNumber}`)
+    .then((res) => res.json())
+    .then((result) => {
+      const { data, total } = result;
+      data.forEach((cat) => {
+        catList.appendChild(createCatItem(cat.date, cat.url));
+      });
+      const numberOfPages = Math.ceil(total / ITEMS_PER_PAGE);
+
+      const minRange = currentPage - PAGES_OFFSET;
+      const maxRange = currentPage + PAGES_OFFSET;
+
+      for (let i = 0; i < numberOfPages; i++) {
+        if (i + 1 >= minRange && i + 1 <= maxRange) {
+          console.log(i + 1, minRange, maxRange);
+          pageContainer.appendChild(createPageItem(i + 1));
+        }
+      }
+    })
+    .finally(() => {
+      assignPagination();
+    });
+};
+
+fetchPage(currentPage);
