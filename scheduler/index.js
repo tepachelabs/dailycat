3f0d80a0a8b7f5407db8733c041104e29b1a2f6a
imariel2d <imariel2d@gmail.com>
2023-05-29 19:51:01 -0700

    Updating cronjob

--- scheduler/index.js
+++ scheduler/index.js
@@ -7,27 +7,24 @@ const { Cat } = require('../db/models/cat');
 const fetchRandomCatImage = require('../services');
 const { getRandomQuote } = require('../utils/quotes');
 
-const rule = new schedule.RecurrenceRule();
-
-rule.hour = 0;
-rule.tz = 'Etc/UTC';
-
 // second | minute | hour | day-of-month | month | day of week
-// https://crontab.guru/#0_*/1_*_*_*
-const job = schedule.scheduleJob(rule, function () {
-  fetchRandomCatImage().then((res) => {
-    const { id, url } = res.data[0];
+// https://crontab.guru/every-day
+const job = schedule.scheduleJob('0 0 * * *', function () {
+  fetchRandomCatImage()
+    .then((res) => {
+      const { id, url } = res.data[0];
 
-    const cat = new Cat({
-      id,
-      url,
-      date: Date.now(),
-      quote: getRandomQuote(),
-    });
+      const cat = new Cat({
+        id,
+        url,
+        date: Date.now(),
+        quote: getRandomQuote(),
+      });
 
-    cat
-      .save()
-      .then(() => console.log('cat inserted to db'))
-      .catch((err) => console.log(err));
-  });
+      cat
+        .save()
+        .then(() => console.log('Daily cat inserted!'))
+        .catch((err) => console.log(err));
+    })
+    .finally(() => job.cancel());
 });
