bc232b441950a458915f8dc0a35755305e5957e0
Bruno Oros <hollowxdstorm@gmail.com>
2023-06-13 17:25:25 -0400

    removed views engine and removed pug dependency as no longer needed. Added a custom 404 page that will be called whenever an incorrect route is used

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '/index.html'));
 });
 
+//The 404 Route (ALWAYS Keep this as the last route)
+router.get('*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '../public/404.html'));
+});
 module.exports = router;
