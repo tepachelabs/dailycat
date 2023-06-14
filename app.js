b05e1ce0f1a4034cba0be49e68de5cf123ee9d7d
Vourpal <110882152+Vourpal@users.noreply.github.com>
2023-06-13 21:19:52 -0400

    removed views engine and removed pug dependency as no longer needed. … (#12)
    * removed views engine and removed pug dependency as no longer needed. Added a custom 404 page that will be called whenever an incorrect route is used
    
    * added favicon and changed title in 404html as well as changed header tags
    
    * added NavBar to the 404error page
    
    * fixed headers and removed unecessary styling in 404Style.css
    
    * added home anchor to error page and added css to make the link visible when the page is being used in a mobile format
    
    * h1 changed to h2

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ const { connect } = require('./db');
 // connect db
 connect();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
