15ab1215d95b54a5fb3c2160fff5585a90fb1b88
imariel2d <imariel2d@gmail.com>
2023-07-09 11:17:19 -0700

    Adding ejs pages/partials

--- app.js
+++ app.js
@@ -15,11 +15,14 @@ const { connect } = require('./db');
 // connect db
 connect();
 
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/api', catsRouter);
 app.use('/', indexRouter);
@@ -37,7 +40,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('pages/error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,37 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+// ** Mongo Model
+const { Cat } = require('../db/models/cat');
+
+// ** Utils
+const { PAGINATION_LIMIT } = require('../utils/contants');
+const { catsWithDateFormatted } = require('../utils/date-reformat');
+
 router.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '/index.html'));
+  Cat.find({})
+    .sort({ date: -1 })
+    .limit(PAGINATION_LIMIT)
+    .skip(0)
+    .then((result) => {
+      Cat.countDocuments({}, (err, count) => {
+        res.render('pages/index', {
+          total: count,
+          data: catsWithDateFormatted(result, {
+            cats: result,
+          }),
+        });
+      });
+    })
+    .catch((error) => {
+      res.render('pages/index');
+    });
 });
 
 //The 404 Route (ALWAYS Keep this as the last route)
+
 router.get('*', (req, res) => {
   res.status(404).sendFile(path.join(__dirname, '../public/404.html'));
 });
+
 module.exports = router;
